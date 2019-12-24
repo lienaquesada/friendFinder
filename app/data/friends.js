e5556eabcda55fe9ed57bc4e83e348f22de5aef6
JaneBane <lienaqperez19@gmail.com>
2019-12-23 22:40:21 -0500

    changes to friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,18 @@
 var friendMatch = [
     {
-      name: "Aaron Rodgers",
-      photo: "http://static3.businessinsider.com/image/597a40094528e62c008b4ec6/aaron-rodgers-is-structuring-his-life-like-tom-brady-in-order-to-play-football-into-his-40s.jpg ",
+      name: "Julia Amasen",
+      photo: "https://hips.hearstapps.com/esq.h-cdn.co/assets/cm/15/05/54cad657dfbd6_-_amysmart.jpg?resize=640:*",
       scores: ["2", "2", "2", "2", "2"]
     }, {
-      name: "Aaron Judge",
+      name: "Mike Rodney",
       photo: "http://image.nj.com/home/njo-media/width620/img/yankees_main/photo/mlb-baltimore-orioles-at-new-york-yankees-fa5cf650079734ba.jpg ",
       scores: ["3", "3", "3", "3", "3"]
     }, {
-      name: " Alex Morgan",
+      name: " Nati Christiana",
       photo: "https://i.ytimg.com/vi/LH8RJ9Q0X0M/maxresdefault.jpg ",
       scores: ["1", "1", "1", "1", "1"]
     }, {
-      name: "Christen Press",
+      name: "Christana Max",
       photo: "http://cdn.shopify.com/s/files/1/0661/5963/files/0010_christen_press_large.jpg?9601849381797396010 ",
       scores: ["3", "3", "3", "3", "1"]
     },
